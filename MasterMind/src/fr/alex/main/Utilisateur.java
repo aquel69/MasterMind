2b8835a2064e35d4d814708d020a07289d3d249a
Lardon Alexandre <alexandre.lardon@yahoo.fr>
2019-02-06 16:05:50 +0100

    probleme sur le mode 1 erreur sur le retour resultat

--- MasterMind/src/fr/alex/main/Utilisateur.java
+++ MasterMind/src/fr/alex/main/Utilisateur.java
@@ -23,35 +23,45 @@ public class Utilisateur extends Joueurs {
 		boolean autrePion = false;
 		List<Integer> liste = new ArrayList<>();
 		
+		
+		for (int i = 0; i < pReponse.length(); i++) {
+			if (pReponse.charAt(i) == pCombinaison.charAt(i)) { 
+				this.bienPlace++;
+				liste.add(i);
+			}
+		}
+		/**for(Integer n : liste)
+			System.out.println("liste apres " + n);*/
+		
 		//boucle permettant d'isoler les pions dans la combinaison saisie par l'uitilisateur
 		for (int i = 0; i < pReponse.length(); i++) {
-			liste.clear();
+			
 			enPlace = false;
 			autrePion = false;
 			
-			//compare les deux combinaisonx pour savoir si le pion est à la bonne place
+			//compare les deux combinaisons pour savoir si le pion est à la bonne place
 			if (pReponse.charAt(i) == pCombinaison.charAt(i)) { 
-				
-				this.bienPlace++;
 				enPlace =true;	
-				liste.add(i);
 			}
 			//boucle permettant d'isoler les pions dans la combinaison mystere
 			for(int j = 0; j < pCombinaison.length(); j++) {
 				//comparaison des pions entre les deux combinaisons ajoutant les mal placés
 				if (!enPlace) {	
 					if (pReponse.charAt(i) == pCombinaison.charAt(j)) {
+						
+						for(Integer n : liste)
+							System.out.println("liste " + n);
 						for(Integer n : liste) {
 							if(pReponse.charAt(i) == pCombinaison.charAt(n)) {
 								autrePion = true;
 							}
-							else {
-								liste.add(j);
-							}
+							
 						}
-						if (autrePion==false)
+						if (autrePion==false) {
 							malPlace++;
+							liste.add(j);
 						break;
+						}
 					}
 				}
 			}
